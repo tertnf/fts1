d6b4e3acd43795c2a485b0329d286bb4561dfd34
Nataliia Tertyshna <tertnf@gmail.com>
2022-11-12 23:13:27 +0200

    almost done
    almost done

--- js/details.js
+++ js/details.js
@@ -1,12 +1,36 @@
 // const screenWidth = window.screen.width;
-// console.log(screenWidth);
+
 const details = document.querySelectorAll("details");
 console.log(details);
 console.log(document.documentElement.clientWidth);
-// console.log(details[1].open);
+
 for (let i = 0; i < details.length; i++) {
   if (document.documentElement.clientWidth > 768) {
     details[i].open = true;
     // console.log(details[i].open);
   }
 }
+
+window.addEventListener(
+  "resize",
+  (event) => {
+    for (let i = 0; i < details.length; i++) {
+      if (document.documentElement.clientWidth > 768) {
+        details[i].open = true;
+      }
+    }
+  },
+  false
+);
+
+window.addEventListener(
+  `resize`,
+  (event) => {
+    for (let i = 0; i < details.length; i++) {
+      if (document.documentElement.clientWidth < 768) {
+        details[i].open = false;
+      }
+    }
+  },
+  false
+);
